becd20f0b4a95614bc230373acc0114db43df558
David Benson <davidbenson86@gmail.com>
2016-03-11 11:24:21 -0500

    fixing injection flaw

--- dist/sds-angular-controls.js
+++ dist/sds-angular-controls.js
@@ -1919,13 +1919,36 @@ angular.module('currencyMask', []).directive('currencyMask', function () {
     // For internal use. Manually binds a template using a provided template function, with a fallback to $compile.
     // Needs to be lightweight.
 
-    function dbBindCell ($compile) {
+    function dbBindCell ($compile, $parse) {
+        function getInputs(obj, $scope, safeScope){
+            if (obj && obj.sharedGetter){
+                var  val = obj($scope);
+                if (typeof val === "string"){
+                    val = document.createElement('div').appendChild(document.createTextNode(val)).parentNode.innerHTML;
+                }
+                obj.assign(safeScope, val);
+
+            }else if(obj.inputs) {
+                _.forOwn(obj.inputs, function (child) {
+                    if (child) {
+                        getInputs(child, $scope, safeScope)
+                    }
+                })
+            }
+
+            return safeScope;
+        }
+
         return{
             restrict: 'A',
             link: function ($scope, $element) {
                 if (typeof $scope._col.template === 'function'){
-                    $element.append($scope._col.template($scope));
+                    var safeScope = {};
+                    _.each($scope._col.template.expressions, function (key){
+                        getInputs($parse(key), $scope, safeScope);
+                    });
 
+                    $element.append($scope._col.template(safeScope));
                 }else if(!$element.html().trim()){
                     // template must be wrapped in a single tag
                     var html = angular.element('<span>' + $scope._col.template  + '</span>');
@@ -1941,11 +1964,12 @@ angular.module('currencyMask', []).directive('currencyMask', function () {
             }
         };
     }
-    dbBindCell.$inject = ["$compile"];
+    dbBindCell.$inject = ["$compile", "$parse"];
 
 
     angular.module('sds-angular-controls').directive('dbBindCell', dbBindCell);
 })();
+
 
 
 (function () {
--- src/table-directives/db-bind-cell.js
+++ src/table-directives/db-bind-cell.js
@@ -5,13 +5,36 @@
     // For internal use. Manually binds a template using a provided template function, with a fallback to $compile.
     // Needs to be lightweight.
 
-    function dbBindCell ($compile) {
+    function dbBindCell ($compile, $parse) {
+        function getInputs(obj, $scope, safeScope){
+            if (obj && obj.sharedGetter){
+                var  val = obj($scope);
+                if (typeof val === "string"){
+                    val = document.createElement('div').appendChild(document.createTextNode(val)).parentNode.innerHTML;
+                }
+                obj.assign(safeScope, val);
+
+            }else if(obj.inputs) {
+                _.forOwn(obj.inputs, function (child) {
+                    if (child) {
+                        getInputs(child, $scope, safeScope)
+                    }
+                })
+            }
+
+            return safeScope;
+        }
+
         return{
             restrict: 'A',
-            link: function ($scope, $element, $sanitize) {
+            link: function ($scope, $element) {
                 if (typeof $scope._col.template === 'function'){
-                    $element.append($sanitize($scope._col.template($scope)));
+                    var safeScope = {};
+                    _.each($scope._col.template.expressions, function (key){
+                        getInputs($parse(key), $scope, safeScope);
+                    });
 
+                    $element.append($scope._col.template(safeScope));
                 }else if(!$element.html().trim()){
                     // template must be wrapped in a single tag
                     var html = angular.element('<span>' + $scope._col.template  + '</span>');
@@ -31,3 +54,4 @@
 
     angular.module('sds-angular-controls').directive('dbBindCell', dbBindCell);
 })();
+
